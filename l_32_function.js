7996917036a4fa2bafa17d307435b3eba9cbcd4d
ksu <igonina.design@gmail.com>
2025-03-17 11:22:52 +0100

    function

--- l_32_function.js
+++ l_32_function.js
@@ -68,3 +68,64 @@ console.log (canAccessWebsite(19))
 
 const canAccessWebsite2 = age => age < 18 ? "No": "Yes"
 console.log (canAccessWebsite2(13))
+
+//функция в функции
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+function calculateW (present){
+    return present * KG_IN_USD;
+}
+function calculateKm (distance){
+    return distance * KM_IN_USD;
+}
+function getExchagePrice (present1, present2, distance){
+    const price1 = calculateW (present1);
+    const price2 = calculateW (present2);
+    const distancePrice = calculateKm (distance);
+    return price1 + price2 + distancePrice;
+}
+console.log (getExchagePrice(1, 2, 10));
+
+function getUser(id = 1, name) {
+    console.log(id + ' ' + name);
+}
+getUser('Антон'); //Антон undefind
+
+
+/*
+	Пользователь:
+	- Возраст
+	- Наличие работы
+	- Деньги
+	Нужно проверить может ли он купить новый MacBook за 2000$?
+	Он может брать не только свои деньги, но и взять кредит.
+	Ему дадут 500$, только если ему больше 24-х лет и он
+	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
+	ином случае.
+	Напишите функцию, которая принимает данные пользователя
+	и товара и возвращает true или false;
+*/
+
+const costMacBook = 2000;
+
+function credit(age, work) {
+    if (age > 24 && work){
+        return 500;
+    }else if(age>24){
+        return 100;
+    }else{
+        return 0;
+    }
+}
+
+function canBuy(money, age, work){
+
+    const credit = credit(age, work);
+    const sum = money + credit;
+    let message = sum >= costMacBook ? true : false
+    return message
+}
+
+let answer = canBuy(1500,25,true); 
+
+console.log(answer)
